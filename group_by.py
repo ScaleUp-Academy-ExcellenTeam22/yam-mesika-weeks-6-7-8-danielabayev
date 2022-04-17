9032afc03f0df43d7f0b9e15ae48861e170dc907
daniel or abayev <dan5204@gmail.com>
2022-04-17 15:47:47 +0300

    Add Documentation.

--- group_by.py
+++ group_by.py
@@ -3,6 +3,13 @@ from typing import Optional
 
 
 def group_by(function: Optional[callable], iterable: Iterable) -> dict:
+    """
+    This function receive function and iterable and return dictionary where the keys are the results of the function and
+     the values are the values in the iterables.
+    :param function: Function to apply.
+    :param iterable: Items to run the function on.
+    :return: Dictionary base on the results of the function and the iterable.
+    """
     # return_dict = {}
     # for item in iterable:
     #     if function(item) in return_dict.keys():
