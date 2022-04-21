854f96bea46c3f6e05beafd32d800121f68b5ef6
daniel or abayev <dan5204@gmail.com>
2022-04-21 13:17:37 +0300

    Add documentation and len function to Message class.

--- Message.py
+++ Message.py
@@ -10,8 +10,23 @@ class Message:
         self.body = body
         self.id = message_id
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        This function returns a user-friendly description of the object.
+        :return: The string of the object.
+        """
         return "{sender} send the message: {message}".format(sender=self.sender,message=self.body)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        This function returns a developer-friendly string representation of an object.
+        :return: The string of the object.
+        """
         return self.__str__()
+
+    def __len__(self) -> int:
+        """
+        This function returns a positive integer that represents the length of the message.
+        :return: The message length.
+        """
+        return len(self.body)
